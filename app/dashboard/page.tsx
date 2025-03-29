aaa8ff4ee824ed7c6744b9f149e707a84c9529d9
Raul Gamero <raul.a.gamero@gmail.com>
2025-03-29 19:15:08 +0100

    updating

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default async function DashboardPage() {
     return (
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-4">
-          Welcome, {user.first_name} {user.last_name || "Student"}!
+          Welcome, {user?.first_name} {user?.last_name || "Student"}!
         </h1>
 
         <p className="text-gray-600 dark:text-gray-400 mb-6">Email: {user.email}</p>
